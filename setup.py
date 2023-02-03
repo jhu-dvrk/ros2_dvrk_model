24b392c6981b8222a414519fa43b184633025a00
Anton Deguet <anton.deguet@jhu.edu>
2023-02-02 20:26:16 -0500

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
 import os
-from glob import glob
-from setuptools import setup
-from setuptools import find_packages
+import glob
+import setuptools
 
 package_name = 'dvrk_model'
 
-setup(
-    name=package_name,
-    version='0.0.0',
-    packages=[package_name],
-    data_files=[
+setuptools.setup(
+    name = package_name,
+    version = '0.0.0',
+    packages = [package_name],
+    data_files = [
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'model'), glob('model/*')),
-        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
-        (os.path.join('share', package_name, 'meshes'), glob('meshes/*.stl')),
-        (os.path.join('share', package_name, 'meshes/psm'), glob('meshes/psm/*.*')),
-        (os.path.join('share', package_name, 'meshes/ecm'), glob('meshes/ecm/*.*')),
-        (os.path.join('share', package_name, 'meshes/mtm'), glob('meshes/mtm/*.*')),
+        (os.path.join('share', package_name), glob.glob('launch/*.py')),
+        (os.path.join('share', package_name, 'model'), glob.glob('model/*')),
+        (os.path.join('share', package_name, 'rviz'), glob.glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, 'meshes'), glob.glob('meshes/*.stl')),
+        (os.path.join('share', package_name, 'meshes/psm'), glob.glob('meshes/psm/*.*')),
+        (os.path.join('share', package_name, 'meshes/ecm'), glob.glob('meshes/ecm/*.*')),
+        (os.path.join('share', package_name, 'meshes/mtm'), glob.glob('meshes/mtm/*.*')),
     ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='Laura Connolly',
-    maintainer_email='15lpc1@queensu.ca',
-    description='URDF and STL files for Sensable Omni',
-    license='MIT'
+    install_requires = ['setuptools'],
+    zip_safe = True,
+    maintainer = 'Anton Deguet',
+    maintainer_email = 'anton.deguet@jhu.edu',
+    description = 'URDF and STL files for the dVRK',
+    license = 'MIT'
 )
